13c2a2f47e0620281a03a98dd63c5b42617b33a9
mikha-el82 <mikhael@centrum.cz>
2022-02-08 13:00:27 +0100

    Finished the main project, some slight modifications still to be done.

--- ui.py
+++ ui.py
@@ -23,10 +23,12 @@ class QuizInterface:
                                                      fill=THEME_COLOR)
         # Buttons
         true_img = PhotoImage(file="images/true.png")
-        self.true_btn = Button(image=true_img, bg=THEME_COLOR, highlightthickness=0, bd=0)
+        self.true_btn = Button(image=true_img, bg=THEME_COLOR, highlightthickness=0, bd=0,
+                               command=self.true_pressed)
         self.true_btn.grid(column=0, row=2, padx=20, pady=20, sticky="e")
         false_img = PhotoImage(file="images/false.png")
-        self.false_btn = Button(image=false_img, bg=THEME_COLOR, highlightthickness=0, bd=0)
+        self.false_btn = Button(image=false_img, bg=THEME_COLOR, highlightthickness=0, bd=0,
+                                command=self.false_pressed)
         self.false_btn.grid(column=1, row=2, padx=20, pady=20, sticky="w")
 
         # Initializing next question
@@ -35,7 +37,38 @@ class QuizInterface:
         # Mainloop
         self.window.mainloop()
 
+    # looping through questions
     def get_next_question(self):
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text, text=q_text)
+        self.scoreboard.config(text=f"Score: {self.quiz.score}")
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+            self.buttons_state(ACTIVE)
+        else:
+            self.canvas.itemconfig(self.question_text, text=f"End of the quiz.\nYour score is {self.quiz.score}/10.")
+            self.buttons_state(DISABLED)
+
+    # True button pressed
+    def true_pressed(self):
+        self.buttons_state(DISABLED)
+        self.give_feedback(self.quiz.check_answer("True"))
+
+    # False button pressed
+    def false_pressed(self):
+        self.buttons_state(DISABLED)
+        self.give_feedback(self.quiz.check_answer("False"))
+
+    # giving feedback
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+        self.window.after(1000, self.get_next_question)
+
+    # Disabling / enabling button use
+    def buttons_state(self, state):
+        self.true_btn.config(state=state)
+        self.false_btn.config(state=state)
 
